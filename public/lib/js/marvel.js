9de44f46359b2c73c8f0f674e2e746fc5458fade
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-01 17:00:18 -0500

    setup connection with the api using one character Daredevil, verified that I am getting that object back

--- public/lib/js/marvel.js
+++ public/lib/js/marvel.js
@@ -1,4 +1,19 @@
-
 "use strict";
 
 console.log('JS Linked');
+
+const myAPIKey = '7d2d500239e06c40351cddf95e95e420';
+var marvelObject;
+var marvelOptions = {
+  apikey: myAPIKey
+}
+
+function getMarvelData () {
+  $.get("https://gateway.marvel.com:443/v1/public/characters?name=daredevil",marvelOptions).done(function(data) {
+    marvelObject = data;
+    console.log(marvelObject);
+  }).fail(function(xhr, err, msg) {
+    alert('Failed to connect!');
+  });
+}
+getMarvelData();
