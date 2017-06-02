4f258460690a0b6c11cb0cfb71a292eda894246d
Dewayne Allen <dewayneallen76@gmail.com>
2017-06-01 22:21:08 -0500

    able to get the information from the API displayed onto the page

--- public/lib/js/marvel.js
+++ public/lib/js/marvel.js
@@ -11,9 +11,19 @@ var marvelOptions = {
 function getMarvelData () {
   $.get("https://gateway.marvel.com:443/v1/public/characters?name=daredevil",marvelOptions).done(function(data) {
     marvelObject = data;
+    addCharacter(marvelObject);
     console.log(marvelObject);
+    console.log(marvelObject.data.results[0].thumbnail.path);
+    console.log(marvelObject.data.results[0].description);
   }).fail(function(xhr, err, msg) {
     alert('Failed to connect!');
   });
 }
 getMarvelData();
+
+function addCharacter(marvelObject) {
+  var content = '';
+  content += '<h3>' + marvelObject.data.results[0].name + '</h3>';
+  content += '<p>' + marvelObject.data.results[0].description + '</p>';
+  $('#addCharacter').html(content);
+};
